916978b731c0e70eefc09d709dd8294456bc370b
CaioCosta2JZ <ca1o.stievo@gmail.com>
2024-09-08 19:31:00 -0300

    Adicionado botão de sair no index

---
+++ v 1.1/js/botoesLaterais.js
@@ -0,0 +1,12 @@
+function botaoSair() {
+    var sairButton = document.getElementById('sair');
+
+    sairButton.addEventListener('click', function() {
+        window.location.href = '../pages/paginaInicial.html'
+});
+
+
+}
+
+
+
